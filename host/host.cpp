8718e019fe48eb8f5091c03b2c2513f23a1b11c5
eyusd <eyusd.gaming@gmail.com>
2021-04-07 08:07:07 +0000

    ca marche pas

--- host/host.cpp
+++ host/host.cpp
@@ -1,12 +1,73 @@
 #include <openenclave/host.h>
 #include <stdio.h>
 #include "linereg_u.h"
+// Att
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "quotefile.h"
+
+static bool EnableVerbosePrintf = false;
+
+void myprintf(const char *format, ...)
+{
+    va_list ap;
+    va_start(ap, format);
+    if (EnableVerbosePrintf){
+      printf(format, ap);
+    }
+    va_end(ap);
+}
+
+oe_enclave_t* create_enclave(const char* enclave_path)
+{
+    oe_enclave_t* enclave = NULL;
+    uint32_t flags = 0;
+
+    if (strstr(enclave_path, "debug") != NULL)
+    {
+        flags = OE_ENCLAVE_FLAG_DEBUG;
+    }
+
+    myprintf("Host: Enclave library %s\n", enclave_path);
+    oe_result_t result = oe_create_linereg_enclave(
+        enclave_path,
+        OE_ENCLAVE_TYPE_SGX,
+        flags,
+        NULL,
+        0,
+        &enclave);
+
+    if (result != OE_OK)
+    {
+        myprintf(
+            "Host: oe_create_linereg_enclave failed. %s",
+            oe_result_str(result));
+    }
+    else
+    {
+        myprintf("Host: Enclave successfully created.\n");
+    }
+    return enclave;
+}
+
+void terminate_enclave(oe_enclave_t* enclave)
+{
+    oe_terminate_enclave(enclave);
+    myprintf("Host: Enclave successfully terminated.\n");
+}
+//Fin Att
 
 int main(int argc, const char* argv[])
 {
     oe_result_t result;
     int ret = 1;
     oe_enclave_t* enclave = NULL;
+    // Att vars
+    uint8_t* pem_key = NULL;
+    size_t pem_key_size = 0;
+    uint8_t* remote_report = NULL;
+    size_t remote_report_size = 0;
+    oe_report_t parsed_report = {0};
 
     //Program loop
     char choice;
@@ -31,13 +92,68 @@ int main(int argc, const char* argv[])
         fprintf(stderr, "Usage: %s enclave_image_path [ --simulate  ]\n", argv[0]);
         goto exit;
     }
-    // Create the enclave
-    result = oe_create_linereg_enclave(argv[1], OE_ENCLAVE_TYPE_AUTO, flags, NULL, 0, &enclave);
-    if (result != OE_OK)
+
+    if (argc != 3)
     {
-        fprintf(stderr, "oe_create_linereg_enclave(): result=%u (%s)\n", result, oe_result_str(result));
+        myprintf("\nUsage: genquote_host <path-to-enclave-binary> <json-file-name>\n");
+        myprintf("  - generates remote quote for the enclave\n");
+        myprintf("  - writes remote quote, parsed data and enclave held data to a json file\n\n");
+        return 1;
+    }
+
+    // Create the enclave
+    myprintf("Host: Creating the enclave\n");
+    enclave = create_enclave(argv[1]);
+    if (enclave == NULL)
+    {
         goto exit;
     }
+
+    myprintf("Host: Requesting a remote report and the encryption key from the enclave\n");
+    result = get_remote_report_with_pubkey(
+        enclave,
+        &ret,
+        &pem_key,
+        &pem_key_size,
+        &remote_report,
+        &remote_report_size);
+    if ((result != OE_OK) || (ret != 0))
+    {
+        myprintf(
+            "Host: get_remote_report_with_pubkey failed. %s\n",
+            oe_result_str(result));
+        if (ret == 0)
+            ret = 1;
+        goto exit;
+    }
+
+    myprintf("Host: The enclave's public key: \n%s\n", pem_key);
+
+    myprintf("Host: Parsing the generated report and writing to a local file\n");
+    result = oe_parse_report(remote_report, remote_report_size, &parsed_report);
+    if (result != OE_OK)
+    {
+        myprintf(
+            "Host: oe_parse_report failed. %s\n",
+            oe_result_str(result));
+        if (ret == 0)
+            ret = 1;
+        goto exit;
+    }
+    else
+    {
+        QuoteFile  myQuoteFile (parsed_report, remote_report, remote_report_size, pem_key, pem_key_size);
+
+        printf("    JSON file created: %s\n", argv[2]);
+        myQuoteFile.WriteToJsonFile("./quotes", argv[2]);
+        if (EnableVerbosePrintf) 
+        {
+            myQuoteFile.WriteToJsonFile(stdout);
+        }
+    }
+
+    ret = 0;
+    //Fin create enclave
 
 
     //Run program
@@ -108,9 +224,16 @@ int main(int argc, const char* argv[])
     ret = 0;
 
 exit:
-    // Clean up the enclave if we created one
-    if (enclave)
-        oe_terminate_enclave(enclave);
+    if (pem_key)
+        free(pem_key);
 
+    if (remote_report)
+        free(remote_report);
+
+    myprintf("Host: Terminating enclave\n");
+    if (enclave)
+        terminate_enclave(enclave);
+
+    myprintf("Host:  %s \n", (ret == 0) ? "succeeded" : "failed");
     return ret;
 }
