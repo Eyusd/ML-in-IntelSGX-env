d230fd74e0d53dc23cbe39b50942d0f1cca30dc8
eyusd <eyusd.gaming@gmail.com>
2021-04-08 15:41:23 +0000

    attestation

--- microsoft-azure-attestation-master/intel.sdk.attest.sample/genquotes/runall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# 
-# Script to build, sigg, get quote and export it in JSON format consumable by MAA
-#
-make all SGX_DEBUG=1
-make all SGX_DEBUG=0
-
--- microsoft-azure-attestation-master/sgx.attest.sample/validatequotes.core/runall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# 
-# Script to verify all example remote attestation quotes
-#
-dotnet run ../genquotes/quotes/enclave.info.debug.json              sharedcus.cus.attest.azure.net    false
-dotnet run ../genquotes/quotes/enclave.info.release.json            sharedcus.cus.attest.azure.net    false
-dotnet run ../genquotes/quotes/enclave.info.prodid.json             sharedcus.cus.attest.azure.net    false
-dotnet run ../genquotes/quotes/enclave.info.securityversion.json    sharedcus.cus.attest.azure.net    false
-
