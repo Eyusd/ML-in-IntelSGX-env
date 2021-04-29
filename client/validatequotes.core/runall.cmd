eec6bf9a58ec334346286c81a3cea678d35dde22
eyusd <eyusd.gaming@gmail.com>
2021-04-29 09:55:31 +0000

    fuuuuuuusion ! ha !

--- client/validatequotes.core/runall.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-rem 
-rem script to verify all example remote attestation quotes 
-rem
-dotnet run ../genquotes/quotes/enclave.info.debug.json               sharedcus.cus.attest.azure.net    false
